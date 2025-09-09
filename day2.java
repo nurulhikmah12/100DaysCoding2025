54c0e939c790b22b442d1a6a71f7bda709de1397
nurulhikmah12 <nh0193610@gmail.com>
2025-09-09 13:55:02 +0800

    Create day2.java

---
+++ day2.java
@@ -0,0 +1,16 @@
+public class Main {
+	public static void main(String[] args) {
+        System.out.println("Menggunakan println():");
+        System.out.println("Hello");
+        System.out.println("World");
+
+        System.out.println("\nMenggunakan print():");
+        System.out.print("Hello ");
+        System.out.print("World");
+
+        System.out.println("\n\nMenggunakan printf():");
+        String nama = "Nunu";
+        int umur = 20;
+        System.out.printf("Nama: %s, Umur: %d", nama, umur);
+    }
+}	

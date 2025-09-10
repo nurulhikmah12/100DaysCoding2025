eeca8e8220c932f7859e94b4500690e8335b8adc
nurulhikmah12 <nh0193610@gmail.com>
2025-09-10 16:37:34 +0800

    Create Day.java

---
+++ Day.java
@@ -0,0 +1,12 @@
+// Ini adalah contoh penggunaan komentar
+public class Contoh {
+    public static void main(String[] args) {
+        // Mencetak "Hello, World!" ke console
+        System.out.println("Hello, World!");
+        
+        /*
+        Ini adalah contoh komentar multi-baris
+        yang dapat mencakup beberapa baris
+        */
+    }
+}

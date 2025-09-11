86005e3683b9503486b24ea74cafb8441d069ce8
nurulhikmah12 <nh0193610@gmail.com>
2025-09-11 19:05:54 +0800

    Create Day4.java

---
+++ Day4.java
@@ -0,0 +1,24 @@
+public class day4 {
+	public static void main(String[] args) {
+        System.out.println("Contoh Escape Sequence:");
+        //Newline (\n) pindah baris\enter
+        System.out.println("1. Pindah baris:\nHalo\nDunia");
+        //Tab (\t) memberikan spasi tabulasi
+        System.out.println("2. Tab:\tJava\tProgramming");
+        //(\\) menampilkan karakter Backslash \
+        System.out.println("3. Backslash: C:\\Program Files\\Java");
+        //Double quote (\")menampilkan tanda kutip ganda
+        System.out.println("4. Kutip ganda: \"Belajar Java\"");
+        //Single quote (\')menampilkan tanda kutip tunggal
+        System.out.println("5. Kutip tunggal: \'A\'");
+        //Carriage return (\r)mengembalikan kursor ke awal baris
+        System.out.println("6. carriage return:");
+        System.out.println("Halo Dunia\rJAVA\n");
+        //Backspace (\b) menghapus karakter sebelumnya
+        System.out.println("7. Backspace: Belajarr\b Java");
+        //Form feed (\f)membuat halaman baru (jarang digunakan)
+        System.out.println("8. Form Feed: Halaman 1\fHalaman 2");
+    }
+}
+   
+    

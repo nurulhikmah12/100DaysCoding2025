8a1adca1e4887075e64126526e1e3a395502ed5b
nurulhikmah12 <nh0193610@gmail.com>
2025-09-14 01:54:44 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,12 @@
+public class day7 {
+	public static void main(String[] args) {
+		//Tipe data karakter (Char)
+		char jenisKelamin = 'P'; //P untuk perempuan dan L untuk laki-laki 
+		System.out.println("Jenis Kelamin: "+ jenisKelamin); 
+		
+		//Tipe data Boolean 
+		boolean sayaPerempuan = true; 
+		//True jika saya adalah perempuan, false jika tidak 
+		System.out.println("Apakah saya perempuan? "+ sayaPerempuan);
+	}
+}

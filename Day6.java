d5e2dadcd5c91b2c69cb179da4de30388f8d0924
nurulhikmah12 <nh0193610@gmail.com>
2025-09-13 19:12:22 +0800

    Create Day6.java

---
+++ Day6.java
@@ -0,0 +1,20 @@
+public class day6 {
+  public static void main(String[] args) {
+    //Contoh penggunaan float 
+    float beratBadan = 55.7f;      //Berat badan dalam kg 
+    float nilaiRataRata = 85.5f;   //Nilai rata-rata siswa
+
+    //Contoh penggunaan double
+    double jarakBumiMatahari = 149_600_000.0;  //Jarak dalam kilometer
+     double pi = 3.141592653589793;  
+    double luasLingkaran = pi * (7.0 * 7.0);   //Hitung luas lingkaran radius 7
+
+    //Mencetak Output
+     System.out.println("=== Contoh Float ===");
+     System.out.println("Berat Badan:"+ beratBadan +"kg");
+     System.out.println("Nilai Rata-rata:"+ nilaiRataRata);
+     System.out.println("=== Contoh Double ===");
+     System.out.println("Jarak Bumi ke Matahari:"+ jarakBumiMatahari +"km");
+     System.out.println("Luas Lingkaran dengan Jari-jari 7:"+ luasLingkaran);
+  }
+}

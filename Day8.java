58b706c48ab8a24fa1fc933ed997bf754b9d0526
nurulhikmah12 <nh0193610@gmail.com>
2025-09-15 18:54:13 +0800

    Create Day8.java

---
+++ Day8.java
@@ -0,0 +1,11 @@
+public class day8 {
+  public static void main (String[] args) {
+    //Contoh penggunaan tipe data String 
+    String nama = "Nurul hikmah";
+    String alamat = "Mombi";
+
+    //Mencetak output 
+    System.out.println("Nama:"+ nama);
+    System.out.println("Alamat:"+ alamat);
+  }
+}

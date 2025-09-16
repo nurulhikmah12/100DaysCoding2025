4eb57e121b341efcd9fabf975ba5c09c7d2505b6
nurulhikmah12 <nh0193610@gmail.com>
2025-09-16 20:36:58 +0800

    Update Day9.java

--- Day9.java
+++ Day9.java
@@ -7,12 +7,16 @@ public class day9 {
     //Inisialisasi variabel
     umur = 19; 
     namaLengkap = "Nurul";
+    System.out.println("=== Data sebelum update ===");
+    System.out.println("Umur saya:"+ umur);
+    System.out.println("Nama saya:"+ namaLengkap);
 
     //Update variabel 
     umur = 20;
     namaLengkap = "Nurul hikmah";
 
     //Mencetak output
+    System.out.println("=== Data sesudah update ==="); 
     System.out.println("Umur saya:"+ umur);
     System.out.println("Nama saya:"+ namaLengkap);
   }

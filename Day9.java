a49bb90bb5f4dc2a32efa14ffad410bbda519d76
nurulhikmah12 <nh0193610@gmail.com>
2025-09-16 20:26:13 +0800

    Create Day9.java

---
+++ Day9.java
@@ -0,0 +1,19 @@
+public class day9 {
+  public static void main(String[] args) {
+    //Deklarasi variabel 
+    int umur;
+    String namaLengkap;
+
+    //Inisialisasi variabel
+    umur = 19; 
+    namaLengkap = "Nurul";
+
+    //Update variabel 
+    umur = 20;
+    namaLengkap = "Nurul hikmah";
+
+    //Mencetak output
+    System.out.println("Umur saya:"+ umur);
+    System.out.println("Nama saya:"+ namaLengkap);
+  }
+}

87783d673fdd75c7c3911d5b2682379c36233eba
nurulhikmah12 <nh0193610@gmail.com>
2025-09-17 19:23:58 +0800

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,12 @@
+public class day10 {
+  public static void main(String[] args){
+    final double PI = 3.14;      //jumlah PI
+    final int JUMLAH_HARI= 7;    //jumlah hari dalam 1 minggu 
+    
+    System.out.println("Nilai PI:"+ PI);
+    System.out.println("Jumlahh hari dalam 1 Minggu:"+ JUMLAH_HARI);
+
+    // PI = 3.14159; // ERROR: tidak bisa diubah
+    
+  }
+}

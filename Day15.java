b267f9bc914ac93c07e8fc9ecf83c36562628577
nurulhikmah12 <nh0193610@gmail.com>
2025-09-22 19:50:11 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+public class day15 {
+  public static void main(String[] args) {
+    int a = 30;
+    int b = 15;
+
+    //Penjumalahan 
+    int tambah = a + b;
+    //Pengurangan
+    int kurang = a - b;
+    System.out.println("Hasil penjumlahan: "+ tambah);
+    System.out.println("Hasil pengurangan: "+ kurang);
+
+    
+    
+  }
+}

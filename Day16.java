768b0a338a80abb89aec2799c3779f2a160ed2cc
nurulhikmah12 <nh0193610@gmail.com>
2025-09-23 18:42:01 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+public class day16 {
+  public static void main(String[] args) {
+    int x = 40;
+    int y = 20;
+
+    //Perkalian 
+    int kali = x * y;
+    //Pembagian 
+    int bagi = x / y;
+
+    System.out.println("Hasil perkalian: "+ kali);
+    System.out.println("Hasil pembagian: "+ bagi);
+  }
+}

384404f8cfffc33203e15eed2595c1f5f2ee0037
nurulhikmah12 <nh0193610@gmail.com>
2025-09-24 19:31:06 +0800

    Create Day17.java

---
+++ Day17.java
@@ -0,0 +1,9 @@
+public class day17 {
+  public static void main(String[] args) {
+    int m = 13;
+    int n = 5;
+
+    int hasil = m % n;
+    System.out.println("Hasil modulus: "+ hasil);
+  }
+}

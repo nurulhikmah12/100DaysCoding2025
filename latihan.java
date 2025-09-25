afa0fae175a00cbb487c5a36c793aff755e2822c
nurulhikmah12 <nh0193610@gmail.com>
2025-09-25 21:42:23 +0800

    Create latihan.java

---
+++ latihan.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class latihan {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Masukkan angka: ");
+    int angka = sc.nextInt();
+
+    if(angka % 2 == 0) {
+      System.out.println("Angka adalah genap");
+    }else{
+      System.out.println("Angka adalah ganjil");
+    }
+  }
+}

18367b77148e67a333c73afd3233391109cced3c
nurulhikmah12 <nh0193610@gmail.com>
2025-09-25 19:20:29 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,14 @@
+public class day18 {
+  public static void main(String[] args) {
+    byte umur = 25;                      //nilai byte
+    int umurInt = umur;                  //otomatis byte ke int
+    long umurLong = umurInt;             //otomatis int ke long
+    double umurDouble = umurLong;        //otomatis long ke double
+
+    System.out.println("Umur (Byte)  : "+ umur );
+    System.out.println("Umur (Int)   : "+ umurInt );
+    System.out.println("Umur (Long)  : "+ umurLong);
+    System.out.println("Umur (Double): "+ umurDouble);
+    
+  }
+}

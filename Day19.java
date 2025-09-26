8313cc3fc0fd95fe3f2ec7b6fa708245758d907b
nurulhikmah12 <nh0193610@gmail.com>
2025-09-26 18:24:08 +0700

    Update Day19.java

--- Day19.java
+++ Day19.java
@@ -1,11 +1,11 @@
 public class day19 {
   public static void main(String[] args) {
-    double angkaD = 99.99;          //tipe data yang menampung nilai yang besar
-    int angkaI = (int) angkaD;      //manual double ke int
-    byte angkaB = (byte) angkaI;    //manual int ke byte
+    double angkaDouble = 99.99;          //tipe data yang menampung nilai yang besar
+    int angkaInteger = (int) angkaDouble;      //manual double ke int
+    byte angkaByte = (byte) angkaInteger;    //manual int ke byte
 
-    System.out.println("Double: "+ angkaD);
-    System.out.println("Integer: "+ angkaI);
-    System.out.println("Byte: "+ angkaB);
+    System.out.println("Double: "+ angkaDouble);
+    System.out.println("Integer: "+ angkaInteger);
+    System.out.println("Byte: "+ angkaByte);
   }
 }

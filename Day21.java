9ab435c7c9ebde4854020626c726eca364712089
nurulhikmah12 <nh0193610@gmail.com>
2025-09-28 19:26:47 +0800

    Update Day21.java

--- Day21.java
+++ Day21.java
@@ -1,19 +1,18 @@
 public class day21 {
-  public static void main(String[] args) {
-    int x = 10;
-    double y = 20.5;
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        
+        System.out.println("Sebelum ditukar: ");
+        System.out.println("Nilai x = "+ x);
+        System.out.println("Nilai y = "+ y);
 
-    System.out.println("Sebelum ditukar: ");
-    System.out.println("Nilai x = "+ x);
-    System.out.println("Nilai y = "+ y);
+        x = x + y;
+        y = x - y;
+        x = x - y;
 
-    //Menggunakan variabel sementara bertipe double
-    double temp = x;      //x dipromosikan otomatis ke double
-    x = (int) y;          //y dikonversi ke int saat masuk ke x          
-    y = temp;             //temp (double) masuk ke y 
-
-     System.out.println("\nSetelah ditukar: ");
-     System.out.println("Nilai x = "+ x);
-     System.out.println("Nilai y = "+ y);
-  }
+        System.out.println("\nSetelah ditukar: ");
+        System.out.println("Nilai x = "+ x);
+        System.out.println("Nilai y = "+ y);
+    }
 }

d5059d999a5cf6d94d22a562aaad3ddbd0de9289
nurulhikmah12 <nh0193610@gmail.com>
2025-09-29 20:12:02 +0800

    Create Day22.java

---
+++ Day22.java
@@ -0,0 +1,14 @@
+public class day22 {
+  public static void main(String[] args) {
+    //Sisi persegi
+    int sisi = 20;
+
+    //Menghitung luas persegi
+    int luas = sisi * sisi;
+
+    //Menampilkan hasil
+    System.out.println("Sisi persegi = "+ sisi);
+    System.out.println("Luas persegi = "+ luas);
+    
+  }
+}

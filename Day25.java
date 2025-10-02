370a2d9c40e00f1fac1febf47da706f55c75a4e7
nurulhikmah12 <nh0193610@gmail.com>
2025-10-02 19:01:15 +0800

    Create Day25.java

---
+++ Day25.java
@@ -0,0 +1,13 @@
+public class day25 {
+    public static void main(String[] args) {
+        int x = 5;
+        System.out.println("Nilai x sebelum increment: " + (x)); 
+        System.out.println("x++ = " + x++); // Output: 5
+        System.out.println("Nilai x setelah increment: " + x); // Output: 6
+
+        int y = 5;
+        System.out.println("\nNilai y sebelum increment: " + (y)); 
+        System.out.println("++y = " + ++y); // Output: 6
+        System.out.println("Nilai y setelah increment: " + y); // Output: 6
+    }
+}

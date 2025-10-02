3d8d62bef08a3bd1a0efa6ad535ae3c21ab2dc0a
nurulhikmah12 <nh0193610@gmail.com>
2025-10-02 21:21:40 +0800

    Update Day25.java

--- Day25.java
+++ Day25.java
@@ -1,13 +1,10 @@
-public class day25 {
+public class Main {
     public static void main(String[] args) {
         int x = 5;
-        System.out.println("Nilai x sebelum increment: " + (x)); 
-        System.out.println("x++ = " + x++); // Output: 5
-        System.out.println("Nilai x setelah increment: " + x); // Output: 6
+        x++; // increment
+        System.out.println(x); // Output: 6
 
-        int y = 5;
-        System.out.println("\nNilai y sebelum increment: " + (y)); 
-        System.out.println("++y = " + ++y); // Output: 6
-        System.out.println("Nilai y setelah increment: " + y); // Output: 6
+        x--; // decrement
+        System.out.println(x); // Output: 5
     }
 }

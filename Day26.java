e3ec574a4f81f0ed0a792d946f1e55d74d6ba888
nurulhikmah12 <nh0193610@gmail.com>
2025-10-03 19:05:30 +0800

    Create Day26.java

---
+++ Day26.java
@@ -0,0 +1,16 @@
+public class day26 {
+    public static void main(String[] args) {
+        int x = 5; // penugasan nilai
+        x += 3; // x = x + 3, Operator Penugasan Penjumlahan
+        System.out.println("Operator penugasan penjumlahan: "+ x); 
+
+        x -= 2; // x = x - 2, Operator Penugasan Pengurangan
+        System.out.println("Operator penugasan pengurangan: "+ x); 
+
+        x *= 2; // x = x * 2, Operator Penugasan Perkalian
+        System.out.println("Operator penugasan perkalian: "+ x);
+
+        x /= 2; // x = x / 2, Operator Penugasan Pembagian
+        System.out.println("Operator penugasan pembagian: "+ x); 
+    }
+}

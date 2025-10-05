9577b0b18124aafce9de27265df92467d0159496
nurulhikmah12 <nh0193610@gmail.com>
2025-10-05 20:21:00 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,13 @@
+public class day28 {
+  public static void main(String[] args) {
+    int a = 6; 
+    int b = 6; 
+    int c = 9; 
+
+    System.out.println("a == b: "+ (a == b));
+    System.out.println("a != b: "+ (a != b));
+    System.out.println("a == c: "+ (a == c));
+    System.out.println("a != b: "+ (a != c));
+    
+  }
+}

3333c932180d6d31015f20dac3bee5dda4322cf9
nurulhikmah12 <nh0193610@gmail.com>
2025-10-05 20:24:03 +0800

    Update Day28.java

--- Day28.java
+++ Day28.java
@@ -7,7 +7,7 @@ public class day28 {
     System.out.println("a == b: "+ (a == b));
     System.out.println("a != b: "+ (a != b));
     System.out.println("a == c: "+ (a == c));
-    System.out.println("a != b: "+ (a != c));
+    System.out.println("a != c: "+ (a != c));
     
   }
 }

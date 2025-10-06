73a543385e19147758d02d323c9513ec4edf6168
nurulhikmah12 <nh0193610@gmail.com>
2025-10-06 18:04:20 +0800

    Create Day29.java

---
+++ Day29.java
@@ -0,0 +1,11 @@
+public class day29 {
+  public static void main(String[] args) {
+    int a = 5;
+    int b = 10; 
+
+    System.out.println("a < b: "+ (a < b));
+    System.out.println("a > b: "+ (a > b));
+    System.out.println("b < a: "+ (b < a));
+    System.out.println("b > a: "+ (b > a));
+  }
+}

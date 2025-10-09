30781c22567bafa2ede6ee8f152e78841db9a612
nurulhikmah12 <nh0193610@gmail.com>
2025-10-09 20:55:51 +0800

    Create Day32.java

---
+++ Day32.java
@@ -0,0 +1,12 @@
+public class day32 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = true;
+        boolean c = false;
+
+        System.out.println("a || b: " + (a || b));
+        System.out.println("a || c: " + (a || c));
+        System.out.println("c || b: " + (c || b));
+        System.out.println("c || c: " + (c || c)); 
+    }
+}

a3c97bb14d4363dd5b69ef2e202f9268c8a3005c
nurulhikmah12 <nh0193610@gmail.com>
2025-10-10 21:10:49 +0800

    Create Day33.javal

---
+++ Day33.java
@@ -0,0 +1,9 @@
+public class day33 {
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+
+        System.out.println("!a: " + !a);
+        System.out.println("!b: " + !b);
+    }
+}

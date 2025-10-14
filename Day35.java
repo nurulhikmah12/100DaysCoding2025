d3202603fdfde5f5ce057a927d97f00e75ec3c81
nurulhikmah12 <nh0193610@gmail.com>
2025-10-14 17:46:18 +0800

    Create Day35.java

---
+++ Day35.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int umur = 25;
+
+        if (umur >= 17) {
+            System.out.println("Anda dapat mengemudi");
+        } else {
+            System.out.println("Anda tidak dapat mengemudi");
+        }
+    }
+}

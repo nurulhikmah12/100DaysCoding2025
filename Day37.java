1f5e5061e057c46920f41ed6cb804bfe7af79e39
nurulhikmah12 <nh0193610@gmail.com>
2025-10-14 22:25:38 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class day37 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan bilangan: ");
+        int bilangan = scanner.nextInt();
+
+        if (bilangan % 2 == 0) {
+            System.out.println(bilangan + " adalah bilangan genap");
+        } else {
+            System.out.println(bilangan + " adalah bilangan ganjil");
+        }
+    }
+}

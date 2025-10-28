5a9e981e652a905bd047fc3b09db46885f2a49f1
nurulhikmah12 <nh0193610@gmail.com>
2025-10-28 15:24:38 +0800

    Create Day51.java

---
+++ Day51.java
@@ -0,0 +1,8 @@
+public class perulannganFor {
+  public static void main(String[] args) {
+    System.out.println("Bilangan 1 sampai 10: ");
+    for (int i = 1; i <= 10; i++){
+      System.out.println(i);
+    }
+  }
+}

86b263d5753db36792c99e96e3e532c0e31565f4
nurulhikmah12 <nh0193610@gmail.com>
2025-10-29 19:32:03 +0800

    Create Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+public class PerulanganWhile {
+    public static void main(String[] args) {
+        int i = 1;
+
+        System.out.println("Bilangan 1 sampai 5:");
+        while (i <= 5) {
+            System.out.println(i);
+            i++;
+        }
+    }
+}

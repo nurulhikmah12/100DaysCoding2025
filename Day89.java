59c5053d4a10a11d9a4bac80544387e2eece7615
nurulhikmah12 <nh0193610@gmail.com>
2025-12-05 20:54:52 +0800

    Add ContohMethodVoid class with void method

---
+++ Day89.java
@@ -0,0 +1,12 @@
+public class ContohMethodVoid {
+
+    // Method void untuk menampilkan pesan
+    public static void tampilkanPesan() {
+        System.out.println("Halo world");
+    }
+
+    public static void main(String[] args) {
+        // Memanggil method void
+        tampilkanPesan();
+    }
+}

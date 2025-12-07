e5a6b44522a073b2359a6fce3c2a65f1e04224cb
nurulhikmah12 <nh0193610@gmail.com>
2025-12-07 20:09:43 +0800

    Add Day91 class with greeting method

---
+++ Day91.java
@@ -0,0 +1,14 @@
+package day85;
+public class day91 {
+
+    // Method dengan parameter
+    public static void sapaPengguna(String nama) {
+        System.out.println("Halo, " + nama + "! Selamat datang.");
+    }
+
+    public static void main(String[] args) {
+        // Memanggil method dengan memberikan argumen
+        sapaPengguna("Nurul hikmah");
+    
+    }
+}

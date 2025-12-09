f320ca4b03ea6b7603cc678872b7cf91b341c9f6
nurulhikmah12 <nh0193610@gmail.com>
2025-12-09 19:52:24 +0800

    Add rounding example in ContohPembulatan class

---
+++ Day93.java
@@ -0,0 +1,13 @@
+public class ContohPembulatan {
+
+    public static void main(String[] args) {
+        
+        double angka1 = 5.3;
+      
+        System.out.println("Angka asli: " + angka1);
+        System.out.println("Ceil : " + Math.ceil(angka1));   // Pembulatan ke atas
+        System.out.println("Floor: " + Math.floor(angka1));  // Pembulatan ke bawah
+        System.out.println("Round: " + Math.round(angka1));  // Pembulatan terdekat
+
+    }
+}

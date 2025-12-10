1dea8af91ec5105fb739e95133bd1e5098e680ce
nurulhikmah12 <nh0193610@gmail.com>
2025-12-10 19:33:39 +0800

    Create Day94.java

---
+++ Day94.java
@@ -0,0 +1,13 @@
+public class CetakDenganMethodVoid {
+
+    // Method void untuk mencetak nama
+    public static void nama() {
+        System.out.println("Nurul hikmah");
+    }
+
+    public static void main(String[] args) {
+
+        // Memanggil method void
+        nama();
+    }
+}
